bfcd7e18ba7e299fbb0184bafd29575907ddc3a1
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-03-10 19:00:47 -0700

    information populates to the screen

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 $(document).ready(function() {
 
-  function createFoodInfoBar() {
-
+  function createFoodInfoBar(food) {
+    let li = $('<li>');
+    $('#infobars').append(li);
+    let header = $('<div>').addClass("collapsible-header").text(food.name);
+    li.append(header);
+    let body = $('<div>').addClass("collapsible-body");
+    li.append(body);
+    for (let dataType in food) {
+      if (dataType === "id" || dataType === "name") {
+        continue;
+      }
+      if (dataType === "options") {
+        console.log(dataType);
+      }
+      body.append($('<span>').text(dataType + ': ' + food[dataType]));
+      body.append($('<br>'));
+    }
   }
 
-  function getFoodInfo() {
+  function populateInfoBars() {
     $.ajax({
       method: 'GET',
       url: 'foods.json',
       dataType: 'json',
       success: (data) => {
-        console.log(data);
+        for (let food of data) {
+          createFoodInfoBar(food);
+        }
       },
       error: function() {
         console.log('error');
@@ -18,7 +35,6 @@ $(document).ready(function() {
     })
   }
 
-  getFoodInfo();
-  console.log("hello");
+  populateInfoBars();
 
 })
